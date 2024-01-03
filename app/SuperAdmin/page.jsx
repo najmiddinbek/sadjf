b225c26cfcc3f9c5f103d1a97b0d588e079727e9
najmiddin <turgunpolatov3gmail.com>
2024-01-03 23:54:15 +0500

    fsa

--- app/SuperAdmin/page.jsx
+++ app/SuperAdmin/page.jsx
@@ -1,32 +1,39 @@
-'use client'
+// 'use client'
 
-import React, { useState } from "react";
-import Image from 'next/image'
-import Prokratura from "../../public/mdm++++ 3.png"
-import Prokratura2 from "../../public/Remove-bg.ai_1700556647313.png"
-import Diagramma from "../../components/Diagramma"
+// import React, { useState } from "react";
+// import Image from 'next/image'
+// import Prokratura from "../../public/mdm++++ 3.png"
+// import Prokratura2 from "../../public/Remove-bg.ai_1700556647313.png"
+// import Diagramma from "../../components/Diagramma"
 
-export default function SuperAdmin() {
+// export default function SuperAdmin() {
 
+//     return (
+//         <>
+//             <div className='super_admin_page'>
+//                 <div className="flex justify-center items-center gap-10 pt-5">
+//                     <Image src={Prokratura2} width={100} height={100} alt='image' />
+//                     <Image src={Prokratura} width={200} height={100} alt='image' />
+//                 </div>
+
+//                 <div className="app">
+//                     <div className="row">
+//                         <div className="flex justify-center">
+//                             {/* <Chart options={chartData.options} series={chartData.series} type="bar" width="1300" /> */}
+//                         </div>
+//                         <Diagramma />
+//                     </div>
+//                 </div>
+//             </div >
+//         </>
+//     )
+// }
+
+
+import React from 'react'
+
+export default function page() {
     return (
-        <>
-            <div className='super_admin_page'>
-                <div className="flex justify-center items-center gap-10 pt-5">
-                    <Image src={Prokratura2} width={100} height={100} alt='image' />
-                    <Image src={Prokratura} width={200} height={100} alt='image' />
-                </div>
-
-                <div className="app">
-                    <div className="row">
-                        <div className="flex justify-center">
-                            {/* <Chart options={chartData.options} series={chartData.series} type="bar" width="1300" /> */}
-                        </div>
-                        <Diagramma />
-                    </div>
-                </div>
-            </div >
-        </>
+        <div>page</div>
     )
 }
-
-
--- components/Diagramma.jsx
+++ components/Diagramma.jsx
@@ -1,215 +1,204 @@
-// 'use client'
+'use client'
 
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const getTopics = async () => {
-//     try {
-//         const res = await fetch('/api/topics', {
-//             cache: 'no-store',
-//         });
-//         if (!res.ok) {
-//             throw new Error('Mavzular yuklanmadi');
-//         }
+const getTopics = async () => {
+    try {
+        const res = await fetch('/api/topics', {
+            cache: 'no-store',
+        });
+        if (!res.ok) {
+            throw new Error('Mavzular yuklanmadi');
+        }
 
-//         return res.json();
-//     } catch (error) {
-//         console.log('Mavzular yuklanishda xatolik: ', error);
-//         return { mavzula: [] };
-//     }
-// };
+        return res.json();
+    } catch (error) {
+        console.log('Mavzular yuklanishda xatolik: ', error);
+        return { mavzula: [] };
+    }
+};
 
 
-// const App = () => {
-//     const [topiclar, setTopiclar] = useState([]);
-//     const [filteredMavzula, setFilteredMavzula] = useState([]);
-//     const [filterValue, setFilterValue] = useState({ newIsm: "", newSinfi: "", school: "" });
-//     const [hide, setHide] = useState(false)
+const App = () => {
+    const [topiclar, setTopiclar] = useState([]);
+    const [filteredMavzula, setFilteredMavzula] = useState([]);
+    const [filterValue, setFilterValue] = useState({ newIsm: "", newSinfi: "", school: "" });
+    const [hide, setHide] = useState(false)
 
 
 
-//     const handleHide = () => {
-//         setHide(!hide)
-//     }
+    const handleHide = () => {
+        setHide(!hide)
+    }
 
 
 
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const a = await getTopics();
-//             const topiclar = a?.topiclar;
+    useEffect(() => {
+        const fetchData = async () => {
+            const a = await getTopics();
+            const topiclar = a?.topiclar;
 
-//             const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
+            const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
 
-//             setTopiclar(filteredTopics);
-//             setFilteredMavzula(filteredTopics);
-//         };
+            setTopiclar(filteredTopics);
+            setFilteredMavzula(filteredTopics);
+        };
 
-//         fetchData();
-//     }, []);
+        fetchData();
+    }, []);
 
-//     const [usersAddedByDate, setUsersAddedByDate] = useState({});
+    const [usersAddedByDate, setUsersAddedByDate] = useState({});
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const usersGroupedByDate = filteredMavzula.reduce((acc, t) => {
-//                 const dateKey = new Date(t.createdAt).toLocaleDateString();
-//                 acc[dateKey] = (acc[dateKey] || 0) + 1;
-//                 return acc;
-//             }, {});
+    useEffect(() => {
+        const fetchData = async () => {
+            const usersGroupedByDate = filteredMavzula.reduce((acc, t) => {
+                const dateKey = new Date(t.createdAt).toLocaleDateString();
+                acc[dateKey] = (acc[dateKey] || 0) + 1;
+                return acc;
+            }, {});
 
-//             setUsersAddedByDate(usersGroupedByDate);
-//         };
+            setUsersAddedByDate(usersGroupedByDate);
+        };
 
-//         fetchData();
-//     }, [filteredMavzula]);
+        fetchData();
+    }, [filteredMavzula]);
 
-//     const [percentageIncreaseByDate, setPercentageIncreaseByDate] = useState({});
+    const [percentageIncreaseByDate, setPercentageIncreaseByDate] = useState({});
 
-//     useEffect(() => {
-//         const calculatePercentageIncrease = () => {
-//             const dates = Object.keys(usersAddedByDate);
-//             const percentageIncrease = {};
+    useEffect(() => {
+        const calculatePercentageIncrease = () => {
+            const dates = Object.keys(usersAddedByDate);
+            const percentageIncrease = {};
 
-//             for (let i = 1; i < dates.length; i++) {
-//                 const currentDate = dates[i];
-//                 const previousDate = dates[i - 1];
+            for (let i = 1; i < dates.length; i++) {
+                const currentDate = dates[i];
+                const previousDate = dates[i - 1];
 
-//                 const currentCount = usersAddedByDate[currentDate];
-//                 const previousCount = usersAddedByDate[previousDate];
+                const currentCount = usersAddedByDate[currentDate];
+                const previousCount = usersAddedByDate[previousDate];
 
-//                 const increasePercentage = ((currentCount - previousCount) / previousCount) * 100;
+                const increasePercentage = ((currentCount - previousCount) / previousCount) * 100;
 
-//                 percentageIncrease[currentDate] = increasePercentage.toFixed(2);
-//             }
+                percentageIncrease[currentDate] = increasePercentage.toFixed(2);
+            }
 
-//             setPercentageIncreaseByDate(percentageIncrease);
-//         };
+            setPercentageIncreaseByDate(percentageIncrease);
+        };
 
-//         calculatePercentageIncrease();
-//     }, [usersAddedByDate]);
+        calculatePercentageIncrease();
+    }, [usersAddedByDate]);
 
-//     const [countSababli, setCountSababli] = useState(0);
+    const [countSababli, setCountSababli] = useState(0);
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const a = await getTopics();
-//             const topiclar = a?.topiclar;
+    useEffect(() => {
+        const fetchData = async () => {
+            const a = await getTopics();
+            const topiclar = a?.topiclar;
 
-//             const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
+            const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
 
-//             setTopiclar(filteredTopics);
-//             setFilteredMavzula(filteredTopics);
-//             const sababliCount = filteredTopics.filter((t) => t.newDarsQoldirish === "Sababli").length;
-//             setCountSababli(sababliCount);
-//         };
+            setTopiclar(filteredTopics);
+            setFilteredMavzula(filteredTopics);
+            const sababliCount = filteredTopics.filter((t) => t.newDarsQoldirish === "Sababli").length;
+            setCountSababli(sababliCount);
+        };
 
-//         fetchData();
-//     }, []);
+        fetchData();
+    }, []);
 
-//     const [countNotSababli, setCountNotSababli] = useState(0);
+    const [countNotSababli, setCountNotSababli] = useState(0);
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const a = await getTopics();
-//             const topiclar = a?.topiclar;
+    useEffect(() => {
+        const fetchData = async () => {
+            const a = await getTopics();
+            const topiclar = a?.topiclar;
 
-//             const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
+            const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
 
-//             setTopiclar(filteredTopics);
-//             setFilteredMavzula(filteredTopics);
-//             const notSababliCount = filteredTopics.filter((t) => t.newDarsQoldirish !== "Sababli").length;
-//             setCountNotSababli(notSababliCount);
-//         };
+            setTopiclar(filteredTopics);
+            setFilteredMavzula(filteredTopics);
+            const notSababliCount = filteredTopics.filter((t) => t.newDarsQoldirish !== "Sababli").length;
+            setCountNotSababli(notSababliCount);
+        };
 
-//         fetchData();
-//     }, []);
+        fetchData();
+    }, []);
 
-//     const [percentageSababli, setPercentageSababli] = useState(0);
-//     const [percentageNotSababli, setPercentageNotSababli] = useState(0);
+    const [percentageSababli, setPercentageSababli] = useState(0);
+    const [percentageNotSababli, setPercentageNotSababli] = useState(0);
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const a = await getTopics();
-//             const topiclar = a?.topiclar;
+    useEffect(() => {
+        const fetchData = async () => {
+            const a = await getTopics();
+            const topiclar = a?.topiclar;
 
-//             const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
+            const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
 
 
-//             setTopiclar(filteredTopics);
-//             setFilteredMavzula(filteredTopics);
+            setTopiclar(filteredTopics);
+            setFilteredMavzula(filteredTopics);
 
-//             // Calculate percentage of items where newDarsQoldirish === "Sababli"
-//             const sababliCount = filteredTopics.filter((t) => t.newDarsQoldirish === "Sababli").length;
-//             const sababliPercentage = (sababliCount / filteredTopics.length) * 100;
-//             setPercentageSababli(sababliPercentage.toFixed(2));
+            // Calculate percentage of items where newDarsQoldirish === "Sababli"
+            const sababliCount = filteredTopics.filter((t) => t.newDarsQoldirish === "Sababli").length;
+            const sababliPercentage = (sababliCount / filteredTopics.length) * 100;
+            setPercentageSababli(sababliPercentage.toFixed(2));
 
-//             // Calculate percentage of items where newDarsQoldirish !== "Sababli"
-//             const notSababliCount = filteredTopics.filter((t) => t.newDarsQoldirish !== "Sababli").length;
-//             const notSababliPercentage = (notSababliCount / filteredTopics.length) * 100;
-//             setPercentageNotSababli(notSababliPercentage.toFixed(2));
-//         };
+            // Calculate percentage of items where newDarsQoldirish !== "Sababli"
+            const notSababliCount = filteredTopics.filter((t) => t.newDarsQoldirish !== "Sababli").length;
+            const notSababliPercentage = (notSababliCount / filteredTopics.length) * 100;
+            setPercentageNotSababli(notSababliPercentage.toFixed(2));
+        };
 
-//         fetchData();
-//     }, []);
+        fetchData();
+    }, []);
 
 
 
 
 
-//     // useEffect(() => {
-//     //     const fetchData = async () => {
-//     //         const a = await getTopics();
-//     //         const topiclar = a?.topiclar;
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const a = await getTopics();
+    //         const topiclar = a?.topiclar;
 
-//     //         const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
+    //         const filteredTopics = topiclar?.filter((t) => t.MFY === '2-sektor') ?? [];
 
-//     //         setTopiclar(filteredTopics);
-//     //         setFilteredMavzula(filteredTopics);
-//     //         const usersGroupedByDate = filteredTopics.reduce((acc, t) => {
-//     //             const dateKey = new Date(t.createdAt).toLocaleDateString();
-//     //             acc[dateKey] = (acc[dateKey] || 0) + 1;
-//     //             return acc;
-//     //         }, {});
+    //         setTopiclar(filteredTopics);
+    //         setFilteredMavzula(filteredTopics);
+    //         const usersGroupedByDate = filteredTopics.reduce((acc, t) => {
+    //             const dateKey = new Date(t.createdAt).toLocaleDateString();
+    //             acc[dateKey] = (acc[dateKey] || 0) + 1;
+    //             return acc;
+    //         }, {});
 
-//     //         setUsersAddedByDate(usersGroupedByDate);
-//     //     };
+    //         setUsersAddedByDate(usersGroupedByDate);
+    //     };
 
-//     //     fetchData();
-//     // }, [filteredMavzula]);
+    //     fetchData();
+    // }, [filteredMavzula]);
 
 
 
 
 
-//     return (
-//         <div className="container">
-//             <div className="flex flex-col justify-start w-full">
-//                 <h2 className="text-3xl text-white font-bold mb-2">Foizdagi o`zgarish</h2>
-//                 {Object.keys(percentageIncreaseByDate).map((date, index) => (
-//                     <p className='text-white' key={date}>
-//                         {date}: {index > 0 ? "Avvalgi kundan farqi " : ""}{percentageIncreaseByDate[date]}%
-//                     </p>
-//                 ))}
-//             </div>
-//             <div className="mb-4">
-//                 <h2 className="text-3xl text-white font-bold mb-2">Sababli dars qoldirilgan o`quvchilar</h2>
-//                 <p className='text-white'>{countSababli} ta o`quvchi sababli dars qoldirgan</p>
-//                 <p className='text-white'>Bu barcha oquvchilarning <b>{percentageSababli}%</b> ni tashkil etadi</p>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default App;
-
-
-
-
-import React from 'react'
-
-export default function Diagramma() {
     return (
-        <div>Diagramma</div>
-    )
-}
+        <div className="container">
+            <div className="flex flex-col justify-start w-full">
+                <h2 className="text-3xl text-white font-bold mb-2">Foizdagi o`zgarish</h2>
+                {Object.keys(percentageIncreaseByDate).map((date, index) => (
+                    <p className='text-white' key={date}>
+                        {date}: {index > 0 ? "Avvalgi kundan farqi " : ""}{percentageIncreaseByDate[date]}%
+                    </p>
+                ))}
+            </div>
+            <div className="mb-4">
+                <h2 className="text-3xl text-white font-bold mb-2">Sababli dars qoldirilgan o`quvchilar</h2>
+                <p className='text-white'>{countSababli} ta o`quvchi sababli dars qoldirgan</p>
+                <p className='text-white'>Bu barcha oquvchilarning <b>{percentageSababli}%</b> ni tashkil etadi</p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
