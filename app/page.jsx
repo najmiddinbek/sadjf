b2ae4285a678cb9c598d1f8e80234932d0185ac8
najmiddin <turgunpolatov3gmail.com>
2024-01-17 12:42:53 +0500

    fasd

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,11 @@ import LoginForm from "../components/LoginForm";
 
 export default function Home() {
   return (
-    <div>
-      <LoginForm />
+    <div className="relative">
+      <div className="absolute  top-0 left-0 w-full z-50 h-screen bg-black">
+        <LoginForm />
+      </div>
+
     </div>
   );
 }
